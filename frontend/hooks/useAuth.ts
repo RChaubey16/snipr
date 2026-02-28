d5b015a1372bb3fc1d12636f6a7d4f9dfe797411
RChaubey16 <ruturajchaubey16@gmail.com>
2026-02-28 12:38:47 +0530

    chore: subdomain auth updates

--- frontend/hooks/useAuth.ts
+++ frontend/hooks/useAuth.ts
@@ -6,32 +6,20 @@ export function useAuth() {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  //   useEffect(() => {
-  //   fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/me`, {
-  //     credentials: "include", // sends cookies with request
-  //   })
-  //     .then((res) => (res.ok ? res.json() : null))
-  //     .then((data) => setUser(data))
-  //     .finally(() => setLoading(false));
-  // }, []);
-
   useEffect(() => {
-    fetch(`/api/auth/me`) // no credentials needed, same domain
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/me`, {
+      credentials: "include", // sends cookies with request
+    })
       .then((res) => (res.ok ? res.json() : null))
       .then((data) => setUser(data))
       .finally(() => setLoading(false));
   }, []);
 
-  //   const logout = async () => {
-  //   await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`, {
-  //     method: "POST",
-  //     credentials: "include",
-  //   });
-  //   setUser(null);
-  // };
-
   const logout = async () => {
-    await fetch(`/api/auth/logout`, { method: "POST" });
+    await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`, {
+      method: "POST",
+      credentials: "include",
+    });
     setUser(null);
   };
 
